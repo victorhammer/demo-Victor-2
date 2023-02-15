bd07f4c3fa8a1414b3034fa213c2c63c0babd1b2
Víctor Alonso <victoralonso987@gmail.com>
2023-02-15 20:47:55 +0100

    Creo un usuario y lo devuelvo por id - 1:25

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,13 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario")
+    public Usuario getUsuario(){
+        Usuario usuario=new Usuario();
+        usuario.setNombre("Víctor");
+        usuario.setApellidos("Alonso");
+        usuario.setEmail("victoralonso@gmail.com");
+        usuario.setTelefono("648702241");
+        return usuario;
     }
 }
---
+++ src/main/java/com/s2daw/demo/models/Usuario.java
@@ -0,0 +1,58 @@
+package com.s2daw.demo.models;
+
+public class Usuario {
+    Long id;
+    String nombre;
+    String apellidos;
+    String email;
+    String telefono;
+    String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
