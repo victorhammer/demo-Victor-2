0f0a6ac450e2ccec0597c38c304caeaf88dbac1c
Víctor Alonso <victoralonso987@gmail.com>
2023-02-15 21:33:13 +0100

    Backend devuelve LA LISTA DE USUARIOS sin BD - 1:55

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="usuario/{id}")
-    public Usuario getUsuario(@PathVariable Long id){
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         usuario.setId(id);
         usuario.setNombre("Víctor");
@@ -22,33 +23,34 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value="usuario45")
-    public Usuario editar(){
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
         Usuario usuario = new Usuario();
+        usuario.setId(1L);
         usuario.setNombre("Víctor");
         usuario.setApellidos("Alonso");
         usuario.setEmail("alumno.53998@ies-azarquiel.es");
         usuario.setTelefono("648702241");
-        return usuario;
-    }
 
-    @RequestMapping(value="usuario343")
-    public Usuario eliminar(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Víctor");
-        usuario.setApellidos("Alonso");
-        usuario.setEmail("alumno.53998@ies-azarquiel.es");
-        usuario.setTelefono("648702241");
-        return usuario;
-    }
+        Usuario usuario2 = new Usuario();
+        usuario.setId(2L);
+        usuario.setNombre("Juliano");
+        usuario.setApellidos("Martin");
+        usuario.setEmail("alumno.12345@ies-azarquiel.es");
+        usuario.setTelefono("612302241");
 
-    @RequestMapping(value="usuario123")
-    public Usuario buscar(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Víctor");
-        usuario.setApellidos("Alonso");
-        usuario.setEmail("alumno.53998@ies-azarquiel.es");
-        usuario.setTelefono("648702241");
-        return usuario;
+        Usuario usuario3 = new Usuario();
+        usuario.setId(3L);
+        usuario.setNombre("Espinaca");
+        usuario.setApellidos("Molina");
+        usuario.setEmail("alumno.56732@ies-azarquiel.es");
+        usuario.setTelefono("612302987");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
     }
 }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 function cargaUsuarios(){
 
-const request = await fetch('usuario/234',{
+const request = await fetch('usuarios',{
     method: 'GET',
     headers: {
         'Accept': 'application/json',
@@ -14,7 +14,16 @@ const request = await fetch('usuario/234',{
         }
     });
     const usuarios = await request.json();
+      let listadoHTML='';
+      for (let usuario of usuarios){
+        let usuarioHTML= '<tr><td>'+usuario.id+
+                         '<td>'+usuario.nombre+
+                         '</td><td>'+usuario.email+
+                         '</td><td>'+usuario.telefono+
+                         '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td><tr>';
+        listadoHTML+=usuarioHTML;
+      }
+      console.log(usuarios);
 
-    console.log(usuarios);
-
+      document.querySelector('#usuarios tbody').outerHTML=listadoHTML;
 }
